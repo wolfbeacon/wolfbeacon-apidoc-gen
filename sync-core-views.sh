0c5b2aae57462d6049a9e4684a831bcaa774850a
bholagabbar <dev.shreyans96@gmail.com>
2017-11-19 17:37:29 +0530

    Added script to sync core docs from dev setup to docgen folder

---
+++ sync-core-views.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while :
+do
+	cp -r /home/bholagabbar/PycharmProjects/wolfbeacon/api/views /home/bholagabbar/Desktop/wolfbeacon_files/wolfbeacon-apidoc-gen/wolfbeacon-core-api/api/views
+	echo "Docs Synced"
+	sleep 2	
+done
